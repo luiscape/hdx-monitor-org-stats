5fdd5bec79b4c5df286be9d9ac9684dc52b25f4b
Luis Capelo <luiscape@gmail.com>
2015-09-18 15:54:26 -0400

    adding functions to calculate average and provide details. closes #1 #2 #3 and #4

--- app/functions/fetch.js
+++ app/functions/fetch.js
@@ -25,15 +25,30 @@ var FetchOrganizationInfo = function (organization_id, callback) {
   }
 
   //
+  // Internal function to organize
+  // users based on their details.
+  //
+  var _user_details = function (data, type) {
+    var details = []
+    var users = data.result.users
+    for (var i = 0; i < users.length; i++) {
+      if (users[i].capacity === type) {
+        details.push(users[i].name)
+      }
+    }
+    return details
+  }
+
+  //
   // Internal function to calculate
   // the number of downloads an
   // organization has.
   //
-  var _calculate_downloads = function (data) {
+  var _calculate_downloads = function (data, property) {
     var downloads = 0
     var datasets = data.result.packages
     for (var i = 0; i < datasets.length; i++) {
-      downloads += datasets[i].tracking_summary.total
+      downloads += datasets[i].tracking_summary[property]
     }
     return downloads
   }
@@ -57,6 +72,20 @@ var FetchOrganizationInfo = function (organization_id, callback) {
     return details
   }
 
+  //
+  // Internal function to calculate
+  // a mean.
+  //
+  var _mean = function (data, property) {
+    var dividend = 0
+    var divisor = data.length
+    for (var i = 0; i < divisor; i++) {
+      dividend += data[i][property].total
+    }
+    var quotient = dividend / divisor
+    return Math.ceil(quotient)
+  }
+
   var payload
   if (organization_id === undefined) {
     payload = {
@@ -73,17 +102,23 @@ var FetchOrganizationInfo = function (organization_id, callback) {
           'organization': organization_id,
           'result': {
             'users': {
-              'count': data.result.users.length,
-              'ids': null,
-              'editors': _user_type(data, 'editor'),
+              'total': data.result.users.length,
               'admins': _user_type(data, 'admin'),
-              'members': _user_type(data, 'member')
+              'editors': _user_type(data, 'editor'),
+              'members': _user_type(data, 'member'),
+              'details': {
+                'admins': _user_details(data, 'admin'),
+                'editors': _user_details(data, 'editor'),
+                'members': _user_details(data, 'member')
+              }
             },
             'datasets': {
-              'count': data.result.package_count
+              'total': data.result.package_count
             },
             'downloads': {
-              'total': _calculate_downloads(data),
+              'total': _calculate_downloads(data, 'total'),
+              'recent': _calculate_downloads(data, 'recent'),
+              'mean': _mean(data.result.packages, 'tracking_summary'),
               'details': _details(data)
             }
           }

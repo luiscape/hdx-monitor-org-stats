92ac9a4241830568ba81df2a524da456073bd416
Luis Capelo <luiscape@gmail.com>
2015-09-21 13:03:17 -0400

    bumping to 0.3.1

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   'CkanInstance': 'https://data.hdx.rwlabs.org',
   'ApiKey': process.env.DEFAULT_API_KEY || null,
-  'version': 'v.0.3.0',
+  'version': 'v.0.3.1',
   'repository': 'https://github.com/luiscape/hdx-monitor-org-stats'
 }

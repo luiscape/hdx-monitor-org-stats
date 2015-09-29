13a6632eff2263ce620bd33928771bcf44070733
Luis Capelo <luiscape@gmail.com>
2015-09-29 12:37:39 -0400

    adding database schemas

--- config/dev.js
+++ config/dev.js
@@ -2,5 +2,22 @@ module.exports = {
   'CkanInstance': 'https://data.hdx.rwlabs.org',
   'ApiKey': process.env.DEFAULT_API_KEY || null,
   'version': 'v.0.3.2',
-  'repository': 'https://github.com/luiscape/hdx-monitor-org-stats'
+  'repository': 'https://github.com/luiscape/hdx-monitor-org-stats',
+  'DataFolder': 'data',
+  'database': [
+    {
+      'name': 'historic',
+      'schema': {
+        'organization': 'TEXT',
+        'date': 'TEXT',
+        'number_of_datasets': 'INTEGER',
+        'total_views': 'INTEGER',
+        'recent_views': 'INTEGER',
+        'mean_views': 'INTEGER',
+        'total_downloads': 'INTEGER',
+        'recent_downloads': 'INTEGER',
+        'mean_downloads': 'INTEGER',
+      }
+    }
+  ]
 }
